bf1d9dc0e345ddef958714a8caad2df79affb476
Kelvin Gomes D'Beraldini <kelvingomes888@gmail.com>
2025-02-28 21:15:54 -0300

    Create BEE1005.py

---
+++ BEE1005.py
@@ -0,0 +1,9 @@
+A = float(input("Digite a primeira nota: "))
+B = float(input("Digite a segunda nota: "))
+
+peso_A = 3.5
+peso_B = 7.5
+
+MEDIA = (A * peso_A + B * peso_B) / (peso_A + peso_B)
+
+print(f"MEDIA = {MEDIA:.5f}")
